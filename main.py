0b47fed487e7019d433a72b316f9c6ef4f77b81d
Omar Cuenca <jesomar.cuenca@gmail.com>
2023-07-16 21:09:09 -0600

    Add dotenv dependency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import getLogger, Args
+from dotenv import load_dotenv, dotenv_values
 
 args = Args.parseArgs()
 
@@ -12,3 +13,11 @@ so that proper init file for logs and args is in place.
 
 Including other imports
 """
+
+load_dotenv()
+
+config = dotenv_values(".env")
+
+logger.info(f"Config: {config}")
+
+logger.info(config["val"])
