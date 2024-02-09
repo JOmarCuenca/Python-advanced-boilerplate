fdc019a24bb5d33915a2f7aa92cb13ae26574c30
Omar Cuenca <jesomar.cuenca@gmail.com>
2024-02-09 13:20:24 -0600

    Disable logging for test cases
    Otherwise they would be logged in the github action space and that isn't a good practice

--- tests/logger_init_test.py
+++
@@ -1,8 +0,0 @@
-from tests import PARAMS, CommonTestCase
-from utils.log_manager import init_log_record
-
-
-class LoggerMethodsTest(CommonTestCase):
-    def test_logger_init(self):
-        # Calling this method 2 times shouldn't fail, but should register an exception
-        init_log_record(**PARAMS)
